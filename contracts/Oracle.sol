f3847b89c4e6b30de87a7fa3411143826bada889
Bulalu <elishabulalu031@gmail.com>
2022-11-29 11:24:22 +0300

    updated scripts and contracts

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -13,11 +13,11 @@ import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol';
 contract Oracle is ChainlinkClient, ConfirmedOwner {
     using Chainlink for Chainlink.Request;
 
-    string public volume;
+    uint256 public volume;
     bytes32 private jobId;
     uint256 private fee;
 
-    event RequestVolume(bytes32 indexed requestId, string volume);
+    event RequestVolume(bytes32 indexed requestId, uint256 volume);
 
     /**
      * @notice Initialize the link token and target oracle
@@ -34,7 +34,7 @@ contract Oracle is ChainlinkClient, ConfirmedOwner {
     constructor() ConfirmedOwner(msg.sender) {
         setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
         setChainlinkOracle(0xCC79157eb46F5624204f47AB42b3906cAA40eaB7);
-        jobId = 'ca98366cc7314957b8c012c72f05aeeb';
+        jobId = '7d80a6386ef543a3abb52817f6707e3b';
         fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
     }
 
@@ -43,14 +43,13 @@ contract Oracle is ChainlinkClient, ConfirmedOwner {
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
     function requestVolumeData() public returns (bytes32 requestId) {
-        Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        Chainlink.Request memory req = buildChainlinkRequest("ca98366cc7314957b8c012c72f05aeeb", address(this), this.fulfill.selector);
 
         // Set the URL to perform the GET request on
-        req.add('get', 'https://www.jsonkeeper.com/b/WJM3');
+        req.add('get', 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD');
 
-        req.add('path', 'name'); // Chainlink nodes 1.0.0 and later support this format
-
-        
+        req.add('path', 'RAW,ETH,USD,VOLUME24HOUR'); // Chainlink nodes 1.0.0 and later support this format
+        req.addInt('times', 10**18);
 
         // Sends the request
         return sendChainlinkRequest(req, fee);
@@ -59,7 +58,7 @@ contract Oracle is ChainlinkClient, ConfirmedOwner {
     /**
      * Receive the response in the form of uint256
      */
-    function fulfill(bytes32 _requestId, string memory  _volume) public recordChainlinkFulfillment(_requestId) {
+    function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId) {
         emit RequestVolume(_requestId, _volume);
         volume = _volume;
     }
--- scripts/deploy_api_contract.py
+++ scripts/deploy_api_contract.py
@@ -5,10 +5,11 @@ from scripts.helpful_scripts import get_account
 
 def deploy_oracle():
     owner = get_account()
-    link_address = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB"
+    link_address = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB" #goerli
+    amount_to_transfer = 0.2 * 10**18
 
     if len(Oracle) == 0:
-        oracle_contract = Oracle.deploy({"from": owner})
+        oracle_contract = Oracle.deploy({"from": owner}, publish_source=False)
         
     else:
         oracle_contract = Oracle[-1]
@@ -16,15 +17,28 @@ def deploy_oracle():
 
     # fund the oracle smart contract with some link tokens
     link_contract = interface.IERC20(link_address)
-    print(f"Elisha's Link balance {link_contract.balanceOf(owner)}")
 
-    link_contract.transfer(oracle_contract, 5 * 10**18 , {"from": owner})
+    print(f"Elisha's Link balance {link_contract.balanceOf(owner)}")
+    print(owner)
+
+    if link_contract.balanceOf(owner) >= amount_to_transfer and link_contract.balanceOf(oracle_contract) < 1 * 10**18:
+        link_contract.transfer(oracle_contract, amount_to_transfer , {"from": owner})
+    else:
+        print("You do not have enough link tokens 🥲, visit the chainlink faucet and get some more 🤑 ")
+        print(f"Your link token balance {link_contract.balanceOf(owner)}, Oracle contract link balance {link_contract.balanceOf(oracle_contract)}")
+
+
+    # assert link_contract.balanceOf(oracle_contract) >= amount_to_transfer
+
 
     # making the api call
-    oracle_contract.requestVolumeData({"from": owner})
+    tx = oracle_contract.requestVolumeData({"from": owner})
+    print(tx.events)
 
     data = oracle_contract.volume()
     print(f"Hey I am the fetched data {data}")
+
+
 
 def main():
     deploy_oracle()
